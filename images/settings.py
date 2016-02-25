1578bdf01d389e1920aa36a14cfae27288e7d340
Mwaleh muturi <alex.mwaleh@andela.com>
2016-02-25 07:21:54 +0300

    (Chore) Setup Database and Social account Login
    - Worked on  login with allauth interface
    - added dj_database
    - removed default database settings and replaced them with dj_database

--- images/settings.py
+++ images/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -99,16 +99,8 @@ if os.getenv('TRAVIS_BUILD', None):
         }
     }
 else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'image',
-            'USER': 'alexmwaleh',
-            'PASSWORD': '',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+    DATABASES['default'] = dj_database_url.config()
+
 AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
     'django_facebook.auth_backends.FacebookBackend',
